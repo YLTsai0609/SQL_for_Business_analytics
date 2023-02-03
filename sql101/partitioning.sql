c2fc09e52619ae280c0a997e9a5263efe378be7d
joetsai <joetsai@17.media>
2023-02-03 21:31:32 +0800

    [Feature] Partitioning Limits for bq

--- sql101/partitioning.sql
+++ sql101/partitioning.sql
@@ -5,3 +5,12 @@
 
 -- unfortunately, we cannot create any partition on bq
 -- or we use integer partition to bypass the question.
+
+
+-- we can leverage IntegerPartitioning to partition each 5 mins
+-- https://medium.com/google-cloud/partition-on-any-field-with-bigquery-840f8aa1aaab
+-- https://cloud.google.com/bigquery/quotas#partitioned_tables
+-- hash the column, provide 10000 partition for a table maximum
+-- needs hash to int64 - 202312050100, 202312052355
+-- 4k for regular partitioned table
+-- 100k for integer range partitioned table
